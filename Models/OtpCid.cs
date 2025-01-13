111084914b23ff84f27728104cb0ae4a434908af
coachnrm <nuttapong_coach@hotmail.com>
2025-01-13 13:14:59 +0700

    add login

---
+++ Models/OtpCid.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace HosxpUi.Models;
+
+public class OtpCid
+{
+    public string Cid {get; set;}
+    public string Otp {get; set;}
+}
--- obj/Debug/net8.0/HosxpUi.AssemblyInfo.cs
+++ obj/Debug/net8.0/HosxpUi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HosxpUi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d97ab274021304e1ffd6620daddc783b3c6913")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76604f0254f8a8b8eb3cbca4fbe62cdbf46295d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HosxpUi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HosxpUi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
