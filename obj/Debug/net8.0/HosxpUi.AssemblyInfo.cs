300ab990b773c8530de0b19eb2085b390ede4c23
coachnrm <nuttapong_coach@hotmail.com>
2025-01-23 12:39:19 +0700

    add redis

--- obj/Debug/net8.0/HosxpUi.AssemblyInfo.cs
+++ obj/Debug/net8.0/HosxpUi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HosxpUi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92bf7e1127cbd77282395463e248313f6104b645")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ca84019b4993e317ab783de3f5f00fbaa28151d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HosxpUi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HosxpUi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
