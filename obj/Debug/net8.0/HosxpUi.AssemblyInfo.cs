22e45692f64b10f42fb8b7760076c6a55e76b549
coachnrm <nuttapong_coach@hotmail.com>
2025-01-23 12:45:30 +0700

    add button delete

--- obj/Debug/net8.0/HosxpUi.AssemblyInfo.cs
+++ obj/Debug/net8.0/HosxpUi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HosxpUi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ca84019b4993e317ab783de3f5f00fbaa28151d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+300ab990b773c8530de0b19eb2085b390ede4c23")]
 [assembly: System.Reflection.AssemblyProductAttribute("HosxpUi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HosxpUi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
