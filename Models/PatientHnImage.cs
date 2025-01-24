c04d76b5e8d608e4cf4378b6847f5483c9960a8d
coachnrm <nuttapong_coach@hotmail.com>
2025-01-24 12:44:12 +0700

    add login without otp

--- Models/PatientHnImage.cs
+++ Models/PatientHnImage.cs
@@ -2,6 +2,9 @@ namespace HosxpUi.Models
 {
     public class PatientHnImage 
     {
+        public string pname { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
         public string hn { get; set; }
         public object pImage { get; set; }
     }
