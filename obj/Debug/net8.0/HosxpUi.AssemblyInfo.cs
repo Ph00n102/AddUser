e2b0551c1d59945a79008709fb722bcc8a27de67
coachnrm <nuttapong_coach@hotmail.com>
2025-03-04 13:34:39 +0700

    add code

--- obj/Debug/net8.0/HosxpUi.AssemblyInfo.cs
+++ obj/Debug/net8.0/HosxpUi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HosxpUi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f773774dc6f934375bfff14b633699e2deafcf55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d73df6d79bb2ec8184659718e68a56b49bb94db1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HosxpUi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HosxpUi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
