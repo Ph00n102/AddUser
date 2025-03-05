4e17f8d8d183084dc128dd31ed926f155b754565
coachnrm <nuttapong_coach@hotmail.com>
2025-03-05 15:22:22 +0700

    add code

---
+++ Models/DrugNameHosxp.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace HosxpUi.Models;
+
+public class DrugNameHosxp
+{
+    public string name { get; set; }
+    public string icode { get; set; }
+    public string sksDfsText { get; set; }
+}
